c738211cf22a38ff843d6ff5d3664e00feac098f
osamikoyo <v.pylayev@mail.ru>
2025-03-24 19:07:30 +0500

    add generate token func

---
+++ internal/modules/user/service/token.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type claims struct{
+	jwt.RegisteredClaims
+	ID uint `json:"id"`
+	Username string `json:"username"`
+}
+
+func generateToken(userId uint, username string) (string, error){
+	claims := claims{
+		ID:   userId,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), // срок действия - 24 часа
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "publics",
+			Subject:   "user_access",
+			ID:        fmt.Sprintf("%d", rand.Uint32()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	return token.SigningString()
+}
--- internal/modules/user/service/user.go
+++ internal/modules/user/service/user.go
@@ -1,8 +1,13 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/osamikoyo/publics/internal/modules/user/entity"
+	"github.com/osamikoyo/publics/internal/modules/user/repository"
 	"github.com/osamikoyo/publics/pkg/logger"
+	"go.uber.org/zap/zapcore"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -13,10 +18,52 @@ type UserService interface {
 }
 
 type userService struct {
-	db     *gorm.DB
+	repo repository.UserRepository
 	logger *logger.Logger
 }
 
-func Init(db *gorm.DB, logger *logger.Logger) {
+func Init(repo repository.UserRepository, logger *logger.Logger) UserService {
+	return &userService{
+		repo: repo,
+		logger: logger,
+	}
+}
+
+func (u *userService) Register(user *entity.User) error {
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil{
+		u.logger.Error("cant register", zapcore.Field{
+			Key: "err",
+			String: err.Error(),
+		})
+
+		return err
+	}
+
+	user.Password = string(password)
+
+	return u.repo.Register(user)
+}
+
+func (u *userService) Login(req *entity.LoginRequest) (string, error) {
+	password, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil{
+		u.logger.Error("cant register", zapcore.Field{
+			Key: "err",
+			String: err.Error(),
+		})
+
+		return "", err
+	}
+
+	user, err := u.repo.Login(req)
+	if err != nil || user == nil{
+		return "", fmt.Errorf("cant auth: %v", err)
+	}
+
+	
+}
+
+func (u *userService) Auth(token string) bool {
 
 }
