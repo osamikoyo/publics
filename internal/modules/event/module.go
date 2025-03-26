cacec22721d501b231e5cceeb8cddf08662fe160
osamikoyo <v.pylayev@mail.ru>
2025-03-26 08:19:56 +0500

    update event module

--- internal/modules/event/module.go
+++ internal/modules/event/module.go
@@ -2,11 +2,31 @@ package event
 
 import (
 	"flamingo.me/dingo"
+	"flamingo.me/flamingo/v3/framework/config"
 	"flamingo.me/flamingo/v3/framework/web"
+	"github.com/osamikoyo/publics/internal/modules/event/entity"
 	"github.com/osamikoyo/publics/internal/modules/event/interfaces"
+	"github.com/osamikoyo/publics/internal/modules/event/repository"
+	service "github.com/osamikoyo/publics/internal/modules/event/service"
+	cfg "github.com/osamikoyo/publics/internal/modules/user/interfaces/config"
+	"github.com/osamikoyo/publics/pkg/logger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Config struct{
+	CompleteConfig config.Map `inject:"config:user"`
+	DSN string `inject:"config:user.dsn"`
+	Key string `inject:"config:user.auth_key"`
+}
+
 type EventModule struct {
+	cfg *Config
+}
+
+func (e *EventModule) Inject(config *Config) *EventModule {
+	e.cfg = config
+	return e
 }
 
 type Routes struct {
@@ -25,6 +45,35 @@ func (r *Routes) Routes(registry *web.RouterRegistry) {
 	registry.HandleGet("ping", r.ping.Ping)
 }
 
-func (e *EventModule) Configure(injector *dingo.Injector) {
-	web.BindRoutes(injector, new(Routes))
+type serviceConfig struct{
+	Key string
+}
+
+func (s *serviceConfig) GetKey() string {
+	return s.Key
+}
+
+func (e *EventModule) Configure(inject *dingo.Injector) {
+	inject.Bind(new(cfg.ServiceConfig)).ToProvider(func () cfg.ServiceConfig {
+		return &serviceConfig{
+			Key: e.cfg.Key,
+		}
+	})
+
+	inject.Bind((*gorm.DB)(nil)).ToProvider(func() (*gorm.DB, error) {
+		db, err := gorm.Open(sqlite.Open(e.cfg.DSN))
+		if err != nil {
+			return nil, err
+		}
+	inject.Bind((*logger.Logger)(nil)).ToProvider(func () *logger.Logger {
+		return logger.Init()
+	})
+
+		return db, db.AutoMigrate(&entity.Event{})
+	})
+
+	inject.Bind(new(repository.EventRepository)).To(new(repository.EventStorage))
+	inject.Bind(new(service.EventService)).To(new(service.EventServiceImpl))
+
+	web.BindRoutes(inject, new(Routes))
 }
