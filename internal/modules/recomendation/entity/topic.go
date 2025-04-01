246fecdbf15aafc95b8c86588fae54bd746dc5b8
osamikoyo <v.pylayev@mail.ru>
2025-04-01 06:28:15 +0500

    add create topic func

--- internal/modules/recomendation/entity/topic.go
+++ internal/modules/recomendation/entity/topic.go
@@ -5,3 +5,25 @@ type Topic struct {
 	TextExplain string `bson:"text_explain"`
 	Desc        string `bson:"desc"`
 }
+
+type GraphTopic struct {
+	UID         string `json:"uid,omitempty"`
+	ID          uint   `json:"id,omitempty"`
+	TextExplain string `json:"text_explain,omitempty"`
+	Desc        string `json:"desc,omitempty"`
+	DgraphType  string `json:"dgraph.type,omitempty"`
+}
+
+func (g *GraphTopic) ToBase() *Topic {
+	return &Topic{
+		ID:          g.ID,
+		TextExplain: g.TextExplain,
+    Desc: g.Desc,
+	}
+}
+
+func (g *GraphTopic) ToGraph() *GraphTopic {
+  return &GraphTopic{
+    UID: ,
+  }
+}
--- internal/modules/recomendation/repository/themes/themes_repo.go
+++ internal/modules/recomendation/repository/themes/themes_repo.go
@@ -1,20 +1,89 @@
 package themes
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/dgraph-io/dgo/v210"
+	"github.com/osamikoyo/publics/internal/modules/recomendation/entity"
 	"github.com/osamikoyo/publics/pkg/logger"
 )
 
 type TopicRepository struct {
-  client *dgo.Dgraph
-  logger *logger.Logger
+	client *dgo.Dgraph
+	logger *logger.Logger
 }
 
 func (repo *TopicRepository) Inject(client *dgo.Dgraph, logger *logger.Logger) *TopicRepository {
-  repo.client = client
-  repo.logger = logger
+	repo.client = client
+	repo.logger = logger
 
-  return repo
+	return repo
 }
 
-func (repo *ThemesRepository) CreateTopic
+func (repo *ThemesRepository) CreateTopic(tc *entity.Topic, alikeID []uint) error {
+	topic := 
+
+  topic.DgraphType = "Topic"
+	topicJSON, err := json.Marshal(topic)
+	if err != nil {
+		return err
+	}
+
+	mu := &api.Mutation{
+		SetJson:   topicJSON,
+		CommitNow: true,
+	}
+
+	ctx := context.Background()
+	resp, err := dgraphClient.NewTxn().Mutate(ctx, mu)
+	if err != nil {
+		return err
+	}
+
+	// Получаем UID новой темы
+	newTopicUID := resp.Uids["blank-0"] // или другой ключ, если задан
+
+	// 2. Если есть alikeIDs, добавляем связи
+	if len(alikeIDs) > 0 {
+		// Формируем запрос для поиска UID тем по их ID
+		query := fmt.Sprintf(`
+			{
+				findTopics(func: eq(id, %d)) @filter(type(Topic)) {
+					uid
+				}
+			}
+		`, alikeID[0]) // Пример для одного ID (можно расширить)
+
+		res, err := dgraphClient.NewTxn().Query(ctx, query)
+		if err != nil {
+			return err
+		}
+
+		var result struct {
+			FindTopics []struct {
+				UID string `json:"uid"`
+			} `json:"findTopics"`
+		}
+
+		if err := json.Unmarshal(res.Json, &result); err != nil {
+			return err
+		}
+
+		if len(result.FindTopics) > 0 {
+			alikeTopicUID := result.FindTopics[0].UID
+
+			edgeMutation := fmt.Sprintf(`
+				<%s> <alike> <%s> .
+			`, newTopicUID, alikeTopicUID)
+
+			_, err = dgraphClient.NewTxn().Mutate(ctx, &api.Mutation{
+				SetNquads: []byte(edgeMutation),
+				CommitNow: true,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
